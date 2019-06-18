c0af9077663d45d125b0afbdc1dfe9a4d9956582
ushaHernandez <ursunina@hotmail.com>
2019-06-18 17:24:41 -0500

    Agregando estilo, cifrado y decifrado.

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,37 @@
 window.cipher = {
-  // ... 
-};
+ encode :(offset,string) => {
+   let result = '';
+   for (let i = 0; i<string.length;i++) {
+     let ascii = string.charCodeAt(i);
+     if(ascii >= 65 && ascii <= 90){
+       let asciiOffset = (ascii-65 + offset) %26 +65;
+       let newtext = String.fromCharCode (asciiOffset);
+       result += newtext;
+     }else if(ascii >= 97 && ascii <=122){
+      let asciiOffset = (ascii-97 + offset) %26 + 97;
+      let newtext = String.fromCharCode (asciiOffset);
+      result += newtext; 
+     }else{
+       result += string[i];
+     }
+   } return result;
+ },
+ decode:(offset,string) => {
+  let result = '';
+  for (let i = 0; i<string.length;i++) {
+    let ascii = string.charCodeAt(i);
+    if(ascii >= 65 && ascii <= 90){
+      let asciiOffset = (ascii-65 - offset +52) %26 + 65;
+      let newtext = String.fromCharCode (asciiOffset);
+      result += newtext;
+    }else if(ascii >= 97 && ascii <=122){
+     let asciiOffset = (ascii-97 - offset +52) %26 + 97;
+     let newtext = String.fromCharCode (asciiOffset);
+     result += newtext; 
+    }else{
+      result += string[i];
+    }
+  } return result;
+
+ }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-const buttonKey=document.getElementById('button-key');
-const showSegundaSeccion=document.getElementById('segunda-seccion');
-const hidePrimeraSeccion=document.getElementById('primera-seccion');
+const buttonInicio = document.getElementById('button-inicio');
+const showSegundaSeccion = document.getElementById('segunda-seccion');
+const hidePrimeraSeccion = document.getElementById('primera-seccion');
+const inputText = document.getElementById('input-text');
+const buttonPadlock = document.getElementById('button-padlock');
+const offset = document.getElementById ('offset-box');
+const buttonKey = document.getElementById ('button-key');
+const buttonClear = document.getElementById ('button-clear');
+
+
 
 const siguientePagina = () => {
-    hidePrimeraSeccion.classList.add('hide');
-    showSegundaSeccion.classList.remove('hide');
-
+hidePrimeraSeccion.classList.add('hide');
+showSegundaSeccion.classList.remove('hide'); 
 }
 
 
-buttonKey.addEventListener('click',siguientePagina);
+buttonInicio.addEventListener('click',siguientePagina);
+buttonPadlock.addEventListener('click', () => {
+    const blankSpace = document.getElementById('exit-box');
+    let offsetValue = parseInt(offset.value);
+    const result = cipher.encode(offsetValue,inputText.value);
+    blankSpace.innerHTML = result;
+});
+
+buttonKey.addEventListener('click', ( ) => {
+    const blankSpace = document.getElementById('exit-box');
+    let offsetValue = parseInt(offset.value);
+    const result = cipher.decode(offsetValue,inputText.value);
+    blankSpace.innerHTML = result;
+});
+
+buttonClear.addEventListener('click',() => {
+    document.getElementById('my-form').reset();
+    document.getElementById('exit-box').innerHTML = '';
+})
